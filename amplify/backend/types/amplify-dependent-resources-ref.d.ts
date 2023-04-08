09669c7ba0b7ff384bcce31b02838c17654b353b
Alexander <amgarofalo@stetson.edu>
2023-04-08 15:28:31 -0400

    03:27 PM 04/08/2023 - Added Amplify backend authentication/security configurations. --Alexander Garofalo

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "qrcodeproject20bc6c87": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
